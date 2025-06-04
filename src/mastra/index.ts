39c25ba01e8a5de3ffbefb6d01420f470ef93169
ニケちゃん / nikechan <o3nike.teg.14@gmail.com>
2025-06-04 08:40:28 +0200

    mastraバージョン更新と不要なDB設定削除

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -7,10 +7,6 @@ import { nikechan } from './agents';
 
 export const mastra = new Mastra({
   agents: { nikechan },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
